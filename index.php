ef69ecb23b00f5588c7b86e236ffe40712ce5912
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-09 18:54:08 +0200

    php arrays

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
-<?php
-  echo 'Hello World!';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+  <?php
+
+$age = [23,16,19,87];
+echo $age[0];
+
+echo "<br>";
+
+$ageAssociative = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+echo $ageAssociative["Peter"];
+
+   ?>
+
+</body>
+</html>
