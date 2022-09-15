20a1623325670a98434314d2a3cc45e879185bcc
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-15 11:25:43 +0200

    added form submitting

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<?php
+if(isset($_POST["submit"])) {
+ $username = $_POST["name"];
+ echo "Hello " . $username;
+};
+
+
+ ?>
+
 <!DOCTYPE HTML>
 <html>
 <body>
 
-<form action="index.php" method="get">
+<form action="index.php" method="post">
 Name: <input type="text" name="name" placeholder="Enter username"><br>
 E-mail: <input type="text" name="email" placeholder="Enter email"><br>
-<input type="submit">
+<input type="submit" name="submit">
 </form>
 
 </body>
