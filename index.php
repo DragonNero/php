74e53480c8cb6428131f0d3d3d6df2fc21a47f4f
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-15 11:39:12 +0200

    name validation with array

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if(isset($_POST["submit"])) {
  $username = $_POST["name"];
  echo "Hello " . $username;
-
+$names = array("Boom");
 $minimum = 4;
 $maximum = 12;
  if(strlen($username) < $minimum){
@@ -11,6 +11,11 @@ $maximum = 12;
  if(strlen($username) > $maximum){
    echo "Username cannot be longer than 12 symbols";
  }
+ if(in_array($username,$names)){
+   echo "You are registered";
+ } else {
+   echo "Sorry you cannot login";}
+
 };
 
 
