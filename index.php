5da08429a53e1dcdbf7f3b1df8eeba7a46bde334
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-15 11:29:56 +0200

    form submitting value check

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 if(isset($_POST["submit"])) {
  $username = $_POST["name"];
  echo "Hello " . $username;
+
+$minimum = 4;
+$maximum = 12;
+ if(strlen($username) < $minimum){
+   echo "Username cannot be less than 4 symbols";
+ }
+ if(strlen($username) > $maximum){
+   echo "Username cannot be longer than 12 symbols";
+ }
 };
 
 
